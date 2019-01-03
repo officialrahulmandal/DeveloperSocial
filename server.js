14583023f1f3d144233a1372a390df84661da0ad
rahul kumar mandal <officialrahulmandal@gmail.com>
2019-01-03 19:06:11 +0530

    basic api routes setup

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const post = require('./routes/api/post')
+const profile = require('./routes/api/profile')
+
 // Db config
 const db = require('./config/keys').mongoURI;
 
@@ -13,6 +18,11 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
+
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',post);
+
 
 const port = process.env.PORT || 5000;
 
