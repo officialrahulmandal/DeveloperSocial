86f5abb2fac47ad2787c574dfc02e0f3d3618d48
rahul kumar mandal <officialrahulmandal@gmail.com>
2019-01-03 17:03:59 +0530

    basic server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by algoscale on 2/1/19.
+ */
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
