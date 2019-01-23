5f68756adbce3968f6aa1761b858e7c972ae3ad5
rahul kumar mandal <officialrahulmandal@gmail.com>
2019-01-23 18:30:25 +0530

    private route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser } from './actions/authActions'
@@ -14,7 +14,10 @@ import Login from '../src/components/auth/Login'
 import Dashboard from './components/dashboard/Dashboard'
 import Register from '../src/components/auth/Register'
 import  { logoutUser } from './actions/authActions'
-import  { clearCurrentProfile } from './actions/profileActions'
+import  { clearCurrentProfile } from './actions/profileActions';
+
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -50,7 +53,10 @@ class App extends Component {
           <div className="container">
               <Route exact path="/register" component={Register}/>
               <Route exact path="/login" component={Login}/>
-              <Route exact path="/dashboard" component={Dashboard}/>
+              <Switch>
+                  <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              </Switch>
+
 
           </div>
               <Footer />
